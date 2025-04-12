6e2e6bb5309b76db190f058346b2b1388fea2d5f
Vadim <vmalyh777@gmail.com>
2025-04-12 18:54:52 +0300

    Убрать аннотацию repository

--- main-service/src/main/java/ru/practicum/user/admin/repository/AdminRepository.java
+++ main-service/src/main/java/ru/practicum/user/admin/repository/AdminRepository.java
@@ -1,12 +1,10 @@
 package ru.practicum.user.admin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import ru.practicum.user.model.User;
 
 import java.util.List;
 
-@Repository
 public interface AdminRepository extends JpaRepository<User, Long> {
     public boolean existsById(Long userId);
 

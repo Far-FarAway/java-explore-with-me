d10c234962885daca84eb6db416acbdce36e2bb4
Vadim <vmalyh777@gmail.com>
2025-04-18 14:59:07 +0300

    Добавить пустой конструктор для корректной сериализации

--- main-service/src/main/java/ru/practicum/category/dto/NewCategoryDto.java
+++ main-service/src/main/java/ru/practicum/category/dto/NewCategoryDto.java
@@ -2,11 +2,15 @@ package ru.practicum.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewCategoryDto {
     @NotBlank
     @NotNull

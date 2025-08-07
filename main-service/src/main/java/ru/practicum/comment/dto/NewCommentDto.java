4432cfb7443fcdaeed7dae17a5af788ad368b6bd
Вадим <vmalyh777@gmail.com>
2025-08-07 07:25:46 +0300

    Исправить ошибку: добавить аннотации валидации ко всем полям

--- main-service/src/main/java/ru/practicum/comment/dto/NewCommentDto.java
+++ main-service/src/main/java/ru/practicum/comment/dto/NewCommentDto.java
@@ -2,6 +2,7 @@ package ru.practicum.comment.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,10 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class NewCommentDto {
     @NotNull
+    @Positive
     Long eventId;
+    @NotNull
+    @Positive
     Long userId;
     @NotNull
     @NotBlank
